f58be32c214908a58a5b5b12f68ba87b99fd7420
ccastri <camilo.castrillon@uao.edu.co>
2023-10-10 23:35:05 -0500

    Backend with nodeJS for FHIR

--- helpers/googleVerify.js
+++ helpers/googleVerify.js
@@ -4,7 +4,8 @@ const client = new OAuth2Client(process.env.Google_ID);
 const googleVerify = async (token) => {
   const ticket = await client.verifyIdToken({
     idToken: token,
-    audience: process.env.Google_ID,  // Specify the CLIENT_ID of the app that accesses the backend
+    audience: process.env.Google_ID,  
+    // Specify the CLIENT_ID of the app that accesses the backend
     // Or, if multiple clients access the backend:
     //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
   });
